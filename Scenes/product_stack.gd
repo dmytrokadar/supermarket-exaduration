f9990a4136c98f1d315bb59d03f85156a4ad3ca0
Dmytro Kadar <dmytrokadar@gmail.com>
2026-04-25 21:58:12 +0200

    added products sway

--- Scenes/product_stack.gd
+++ Scenes/product_stack.gd
@@ -1,6 +1,13 @@
 extends Node3D
 
+@export var UNSTABLE_PRODUCT_COUNT = 20
+
 @export var product_on_cart_prefab: PackedScene
+#@export var max_tilt_degrees := 20.0
+@export var tilt_curve: Curve
+#@export var max_amplitude = .01
+@export var phase_shift = 0.1
+@export var speed = .002
 
 var product_list: Array[Node]
 
@@ -13,9 +20,25 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("spawn_product"):
 		spawn_product(product_on_cart_prefab)
+	
+	slide_stack(Time.get_ticks_msec())
 
-func slide_stack():
-	pass
+
+func slide_stack(time):
+	var max_amplitude = lerp(0.0, 1.0, clampf(float(product_list.size())/UNSTABLE_PRODUCT_COUNT, 0.0, 1.0))
+	
+	for obj in product_list:
+		var heightFactor = float(product_list.find(obj)) / product_list.size()
+		var currentAmplitude = max_amplitude * heightFactor
+		
+		var swayOffset = sin(time * speed - product_list.find(obj) * phase_shift)
+		
+		obj.position.x = currentAmplitude * swayOffset
+		
+		var swayOffsetz = cos(time * speed - product_list.find(obj) * phase_shift)
+		obj.position.z = currentAmplitude * swayOffsetz
+		#print(heightFactor)
+	
 
 func spawn_product(prefab):
 	var inst = prefab.instantiate()
