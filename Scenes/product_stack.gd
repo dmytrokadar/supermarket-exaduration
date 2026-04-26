aaba993fcb122353e2eeaee07ca66210adc7f179
Dmytro Kadar <dmytrokadar@gmail.com>
2026-04-26 15:19:25 +0200

    slide works

--- Scenes/product_stack.gd
+++ Scenes/product_stack.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var UNSTABLE_PRODUCT_COUNT = 20
+@export var PLAYER: Node3D
 
 @export var product_on_cart_prefab: PackedScene
 #@export var max_tilt_degrees := 20.0
@@ -8,45 +9,68 @@ extends Node3D
 #@export var max_amplitude = .01
 @export var phase_shift = 0.1
 @export var speed = .002
+@export var follow_speed: float = 15.0
+@export var MAX_SPEED = 20
+@export var MAX_SLIDE_DISTANCE: float = 0.35
 
-var product_list: Array[Node]
+var rigid_player: RigidBody3D
+
+var product_list: Array[Node3D]
+var product_base_local_positions: Array[Vector3]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	rigid_player = PLAYER as RigidBody3D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("spawn_product"):
 		spawn_product(product_on_cart_prefab)
 	
-	slide_stack(Time.get_ticks_msec())
 
-
-func slide_stack(time):
-	var max_amplitude = lerp(0.0, 1.0, clampf(float(product_list.size())/UNSTABLE_PRODUCT_COUNT, 0.0, 1.0))
+func _physics_process(delta: float) -> void:
+	slide_stack(delta)
 	
-	for obj in product_list:
-		var heightFactor = float(product_list.find(obj)) / product_list.size()
-		var currentAmplitude = max_amplitude * heightFactor
-		
-		var swayOffset = sin(time * speed - product_list.find(obj) * phase_shift)
-		
-		obj.position.x = currentAmplitude * swayOffset
-		
-		var swayOffsetz = cos(time * speed - product_list.find(obj) * phase_shift)
-		obj.position.z = currentAmplitude * swayOffsetz
-		#print(heightFactor)
+
+func slide_stack(delta):
+	if rigid_player == null or product_list.is_empty():
+		return
+
+	var velocity: Vector3 = rigid_player.linear_velocity
+	var velocity_xz := Vector2(velocity.x, velocity.z)
+	var speed_ratio := clampf(velocity_xz.length() / MAX_SPEED, 0.0, 1.0)
+
+	var slide_dir := Vector3.ZERO
+	if velocity_xz.length() > 0.001:
+		slide_dir = Vector3(-velocity_xz.x, 0.0, -velocity_xz.y).normalized()
+
+	var blend := clampf(follow_speed * delta, 0.0, 1.0)
+	var count := product_list.size()
+
+	for i in range(count):
+		var obj := product_list[i]
+		if obj == null:
+			continue
+
+		var height_factor := float(i + 1) / float(count)
+		var slide_amount := MAX_SLIDE_DISTANCE * speed_ratio * height_factor
+		var target_global := to_global(product_base_local_positions[i]) + slide_dir * slide_amount
+
+		obj.global_position = obj.global_position.lerp(target_global, blend)
 	
 
 func spawn_product(prefab):
-	var inst = prefab.instantiate()
+	var inst := prefab.instantiate() as Node3D
+	if inst == null:
+		return
+
 	add_child(inst)
-	var box = inst as CSGBox3D
+	var box := inst as CSGBox3D
 	inst.position =\
 		Vector3(0, 0, 0) if product_list.is_empty() else product_list.back().position +\
 		 Vector3(0, box.size.y, 0)
 	product_list.append(inst)
+	product_base_local_positions.append(inst.position)
 	
 	print("product_spawned", (inst.position))
